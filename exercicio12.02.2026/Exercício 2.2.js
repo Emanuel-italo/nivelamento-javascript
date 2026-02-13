c29cd7de5c4eef35c8959158ed9892d76b6981c4
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-12 22:40:27 -0300

    resolvendo o bloco dois do exericio passado, aqui eu aprendi sobre o type of, boleano e undefined. o typeof ele trás sobre o que cada variavel representa e o que ela é se é uma string ou um numero, ou uma condição, além disso aprendi sobre o let que você passando ele quando vai submeter uma variavel, ele permite com que você mude diferente do const.

---
+++ exercicio12.02.2026/Exercício 2.2.js
@@ -0,0 +1,11 @@
+const nome = "Emanuel";
+let idade = "24";
+const ativo = true;
+
+let semValor = undefined; 
+
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof ativo);
+console.log(semValor);
---
+++ exercicio12.02.2026/exercicio2.1.js
@@ -0,0 +1,8 @@
+const nomeJogador = "Ana";
+console.log(nomeJogador)
+
+let pontuacao = 0;
+console.log(pontuacao)
+
+pontuacao = 10;
+console.log(pontuacao)
