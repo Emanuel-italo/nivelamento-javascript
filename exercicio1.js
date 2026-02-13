37706f08521b85c0932d03c1d18b944607d9f322
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-12 22:05:13 -0300

    resolvendo o exercicio 1! aprendendo a declaravar variaveis, e a chama-las na saida dos prints com console.log

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+const nome = "Emanuel";
+const curso = "Analise e desenvolvimento de sistemas";
+const frase = "Estou aprendendo JavaScript";
+
+
+console.log(nome)
+console.log(curso)
+console.log(frase)
