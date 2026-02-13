3bbb75f9f9016110bfcc819957d1f7dc6dc5ce45
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-12 22:07:39 -0300

    colocando frase dois e aprendendo a chamada das variaveis, no console.log - Aula 12/02/2026

--- exercicio1.js
+++ exercicio1.js
@@ -1,8 +1,9 @@
 const nome = "Emanuel";
 const curso = "Analise e desenvolvimento de sistemas";
 const frase = "Estou aprendendo JavaScript";
-
+const frase2 = "gostando de JavaScript";
 
 console.log(nome)
 console.log(curso)
 console.log(frase)
+console.log(frase2)
