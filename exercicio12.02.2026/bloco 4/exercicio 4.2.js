e5f80ec6e9f25241ef385830de69693d93467541
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-16 13:36:24 -0300

    trabalhando com dicionario no js, acessando o dicionario, verificando as chaves dos valores que foram atribuidos . 12/02/2026

---
+++ exercicio12.02.2026/bloco 4/exercicio 4.2.js
@@ -0,0 +1,16 @@
+let veiculo = {
+
+    marca: "porche",
+
+    modelo: "porche",
+
+    ano: 2001 ,
+
+    disponivel: true
+
+};
+
+console.log(veiculo.marca);
+console.log(veiculo["modelo"]);
+veiculo.cor = "preto";
+console.log(veiculo.cor);
