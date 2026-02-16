bcd1b5e6ec3f3101abd2325e748cfca43964919e
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-02-16 12:14:21 -0300

    aprendendo sobre listas dentro do js, adicionando 3 objetos dentro da lista, para mais tarde fazer o print do primeiro objeto do segundo objeto e chamar a funcionalidade para adicionar mais uma coisa na linha e exibir o ultimo item adicionado. 12/02/2026

---
+++ exercicio12.02.2026/bloco 4/exercicio4.1.js
@@ -0,0 +1,16 @@
+let tarefas = ["Oracao", "determinacao","impulsionamentacao"];
+
+//acessando o primeiro icone da lista.
+console.log(tarefas[0]);
+
+//acessando o ultimo icone da lista
+console.log(tarefas[2]);
+
+
+console.log(tarefas.length);
+
+tarefas.push("python")
+
+console.log(tarefas.length);
+console.log(tarefas[3]); 
+
